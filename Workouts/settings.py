3597d20be2c2eb022567cb7cafe255dca1c356ca
jhoanVasquez <jhoan.vasquez@correounivalle.edu.co>
2021-08-14 15:13:56 -0500

    ajustes

--- Workouts/settings.py
+++ Workouts/settings.py
@@ -96,7 +96,7 @@ WSGI_APPLICATION = 'Workouts.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {  
-    'default': {
+    'default': { 
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'workout',
 	    'USER': 'postgres',
