59a34fcd0f7e2304ebdde377872530201e782ddc
marlon-morales <marlon.morales@correounivalle.edu.co>
2021-10-15 19:22:32 -0500

    desbordando corregido

--- WorkoutsApp/recomendador.py
+++ WorkoutsApp/recomendador.py
@@ -80,7 +80,7 @@ def alg_Cal(val_alg, EjerciciosDF):
     val_2= (EjerciciosDF.at[(1),'calificacion'])*porce_cali + (val_alg[1][1])*porce_alg
     val_alg_cal = [val_1,val_2]
 
-    for i in range (2,len(EjerciciosDF.index)) :
+    for i in range (2,len(EjerciciosDF.index)-1) :
           
         val = (EjerciciosDF.at[(i),'calificacion'])*porce_cali + (val_alg[i][1])*porce_alg
         val_alg_cal.append(val)
